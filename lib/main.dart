10bfc1be5fc4069e9638193b432a8852a7701eeb
emtalen <59577366+emtalen@users.noreply.github.com>
2020-07-01 11:48:45 +0200

    adding basic functionality to the buttons

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
       'What\'s your favorite color?',
       'What\'s your favorite animal?'
     ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -17,9 +22,9 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             Text('The question!'),
-            RaisedButton(child: Text('Answer 1'), onPressed: null),
-            RaisedButton(child: Text('Answer 3'), onPressed: null),
-            RaisedButton(child: Text('Answer 3'), onPressed: null),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
           ],
         ),
       ),
